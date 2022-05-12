65960f11f36d64dc47a3df0e3cdc6e21468577a9
Ryan Lok <rlok.pc@gmail.com>
2022-05-12 15:24:22 +0800

    Add black and rainbow brush functionality

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 let drawingField = document.querySelector('.drawingField');
-let btn = document.querySelector('button');
+let newCanvasBtn = document.querySelector('.newCanvas');
+let blackBrushBtn = document.querySelector('.blackBrush');
+let pencilBtn = document.querySelector('.pencil');
+let rainbowBtn = document.querySelector('.rainbowBrush')
 
+let currentBrush = 'black'
+
+
+// generate 16 x 16 grid on page load
 for (i = 1; i <= 16 * 16; i++) {
     const square = document.createElement('div');
     square.style.height = "50px";
@@ -11,12 +18,37 @@ for (i = 1; i <= 16 * 16; i++) {
     drawingField.appendChild(square);
 }
 
-function changeColor(e) {
-    let customColor = randomColor()
-    this.style.background = `${customColor}`;
+// eventListeners and functions to change brush selection
+
+function changeBlackBrush() {
+    currentBrush = "black";
 }
 
-btn.addEventListener('click', newGrid)
+function changeRainbowBrush() {
+    currentBrush = "rainbow";
+}
+
+function changePencil() {
+    currentBrush = "pencil";
+}
+
+blackBrushBtn.addEventListener('click', changeBlackBrush);
+pencilBtn.addEventListener('click', changePencil);
+rainbowBtn.addEventListener('click', changeRainbowBrush);
+
+function changeColor(e) {
+
+    if (currentBrush === "rainbow") {
+        let customColor = randomColor()
+        this.style.background = `${customColor}`;
+    } else if (currentBrush === "black") {
+        this.style.background = "black";
+    } else if (currentBrush === "pencil") {
+    }
+    
+}
+
+newCanvasBtn.addEventListener('click', newGrid)
 
 function newGrid() {
     squaresPerSide = prompt('How many squares per side? (Max 100)');
